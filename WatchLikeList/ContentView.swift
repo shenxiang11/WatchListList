3a99862d6c1edd312102a93566fb7a4a39fe0acf
shenxiang <shenxiang@foursmile001.com>
2024-01-22 13:56:49 +0800

    优化代码

--- WatchLikeList/ContentView.swift
+++ WatchLikeList/ContentView.swift
@@ -24,41 +24,18 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             VStack {
-                GeometryReader { mainView in
-                    let my = mainView.frame(in: .global).minY
-                    
-                    ScrollView {
-                        VStack(spacing: 20) {
-                            ForEach(exampleData, id: \.self) { color in
-                                GeometryReader { itemView in
-                                    let iy = itemView.frame(in: .named("List")).minY
-                                    
-                                    RowView(color: color)
-                                        .scaleEffect(calc(y1: my, y2: iy), anchor: .bottom)
-                                        .opacity(calc(y1: my, y2: iy))
-                                }
-                                .frame(height: 140)
-                            }
+                ScrollView {
+                    VStack(spacing: 20) {
+                        ForEach(exampleData, id: \.self) { color in
+                            RowView(color: color)
                         }
-                        .padding(PADDING)
                     }
+                    .padding(PADDING)
                 }
+                .coordinateSpace(.named("List"))
             }
-            .coordinateSpace(.named("List"))
             .navigationTitle("列表")
             .navigationBarTitleDisplayMode(.inline)
-        }
-    }
-    
-    func calc(y1: CGFloat, y2: CGFloat) -> CGFloat {
-        var result = (y2 + y1 - PADDING) / y1
-        
-        if (result > 1) {
-            return 1
-        } else if (result < 0) {
-            return 0
-        } else {
-            return result
         }
     }
 }
--- WatchLikeList/RowView.swift
+++ WatchLikeList/RowView.swift
@@ -8,14 +8,59 @@
 import SwiftUI
 
 struct RowView: View {
+    @State var y: CGFloat = 0
     var color: UIColor = .blue
     
     var body: some View {
         Rectangle()
             .foregroundStyle(Color(color))
             .clipShape(.rect(cornerRadius: 25))
+            .frame(height: 140)
+            .scaleEffect(calc(y2: y), anchor: .bottom)
+            .opacity(calc(y2: y))
+            .offsetExtractor(coordinateSpace: "List") { value in
+                y = value.minY
+            }
+    }
+    
+    func calc(y2: CGFloat) -> CGFloat {
+        let k: CGFloat = 100
+        let result = (y2 + k - PADDING) / k
+        
+        if (result > 1) {
+            return 1
+        } else if (result < 0) {
+            return 0
+        } else {
+            return result
+        }
     }
 }
+
+struct OffsetKey: PreferenceKey {
+    static var defaultValue: CGRect = .zero
+    static func reduce(value: inout CGRect, nextValue: () -> CGRect) {
+        value = nextValue()
+    }
+}
+
+extension View {
+    func offsetExtractor(coordinateSpace: String, completion: @escaping (CGRect) -> ()) -> some View {
+        self
+            .overlay {
+                GeometryReader { proxy in
+                    let rect = proxy.frame(in: .named(coordinateSpace))
+                    
+                    Color.clear
+                        .preference(key: OffsetKey.self, value: rect)
+                        .onPreferenceChange(OffsetKey.self) {
+                            completion($0)
+                        }
+                }
+            }
+    }
+}
+
 
 #Preview {
     RowView()
