4a56674e468a9f380e3a0ef6b0154c7af15737f7
shenxiang <shenxiang@foursmile001.com>
2024-01-22 10:51:10 +0800

    Initial Commit

---
+++ WatchLikeList/WatchLikeListApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchLikeListApp.swift
+//  WatchLikeList
+//
+//  Created by FS on 2024/1/22.
+//
+
+import SwiftUI
+
+@main
+struct WatchLikeListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
